efbe4cc7bc52a5ade9b3feeab05fcf913a4ad392
Nhan Tran <nhan210605@gmail.com>
2026-03-05 15:57:52 +0700

    Giai đoạn 4 bài tập - hoàn thành

--- QuanLyBanHang/Data/HoaDon_ChiTiet.cs
+++ QuanLyBanHang/Data/HoaDon_ChiTiet.cs
@@ -1,4 +1,6 @@
-﻿namespace QuanLyBanHang.Data
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuanLyBanHang.Data
 {
     public class HoaDon_ChiTiet
     {
@@ -10,5 +12,17 @@
 
         public virtual HoaDon HoaDon { get; set; } = null!;
         public virtual SanPham SanPham { get; set; } = null!;
+    }
+
+    [NotMapped]
+    public class DanhSachHoaDon_ChiTiet
+    {
+        public int ID { get; set; }
+        public int HoaDonID { get; set; }
+        public int SanPhamID { get; set; }
+        public string TenSanPham { get; set; } // Thêm 
+        public short SoLuongBan { get; set; }
+        public int DonGiaBan { get; set; }
+        public int ThanhTien { get; set; } // Thêm 
     }
 }
