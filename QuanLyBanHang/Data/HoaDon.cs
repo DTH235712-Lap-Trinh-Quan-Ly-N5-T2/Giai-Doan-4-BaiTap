facb67f12bd4db2626539b2ff260299a54305845
Nhan Tran <nhan210605@gmail.com>
2026-03-05 15:24:21 +0700

    Giai đoạn 4 part 1

---
+++ QuanLyBanHang/Data/HoaDon.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace QuanLyBanHang.Data
+{
+    public class HoaDon
+    {
+        public int ID { get; set; }
+        public int NhanVienID { get; set; }
+        public int KhachHangID { get; set; }
+        public DateTime NgayLap { get; set; }
+        public string? GhiChuHoaDon { get; set; }
+
+        public virtual ObservableCollectionListSource<HoaDon_ChiTiet> HoaDon_ChiTiet { get; } = new();
+        public virtual KhachHang KhachHang { get; set; } = null!;
+        public virtual NhanVien NhanVien { get; set; } = null!;
+    }
+}
